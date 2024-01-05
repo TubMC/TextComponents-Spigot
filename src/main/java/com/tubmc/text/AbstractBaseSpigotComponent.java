e99ddc123e75b675ca3382d219e79932f17b78f1
BradBot_1 <BradBot_1@outlook.com>
2024-01-05 00:04:20 +0000

    Add missing headers

--- src/main/java/com/tubmc/text/AbstractBaseSpigotComponent.java
+++ src/main/java/com/tubmc/text/AbstractBaseSpigotComponent.java
@@ -27,6 +27,27 @@ import net.md_5.bungee.api.chat.hover.content.Entity;
 import net.md_5.bungee.api.chat.hover.content.Item;
 import net.md_5.bungee.api.chat.hover.content.Text;
 
+/**
+ *    Copyright 2023-2024 TubMC.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+/**
+ * 
+ * @author BradBot_1
+ * @since 1.0.0
+ * @version 1.0.0
+ */
 @Internal
 sealed abstract class AbstractBaseSpigotComponent<T extends BaseComponent> implements ImplementationComponentBase, ITypedProxy<T> permits KeybindSpigotComponent, LiteralSpigotComponent, ScoreboardSpigotComponent, SelectedSpigotComponent, TranslatableSpigotComponent {
 	
--- src/main/java/com/tubmc/text/SpigotTextImplementation.java
+++ src/main/java/com/tubmc/text/SpigotTextImplementation.java
@@ -15,6 +15,27 @@ import net.md_5.bungee.api.chat.SelectorComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.TranslatableComponent;
 
+/**
+ *    Copyright 2023-2024 TubMC.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+/**
+ * 
+ * @author BradBot_1
+ * @since 1.0.0
+ * @version 1.0.0
+ */
 @Internal
 final class SpigotTextImplementation extends AbstractImplementation {
 	
